b455e91017509dc61484a426af4e83ab06723a06
M. Sonntag <michael.p.sonntag@gmail.com>
2015-08-07 15:21:39 +0200

    initial commit

---
+++ src/main/java/org/g_node/App.java
@@ -0,0 +1,13 @@
+package org.g_node;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
