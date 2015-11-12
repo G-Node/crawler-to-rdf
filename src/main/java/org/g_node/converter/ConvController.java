91b22a65a82314b5a9dd48cb0be12030abe6d35e
M. Sonntag <michael.p.sonntag@gmail.com>
2015-11-12 18:32:44 +0100

    convRDF: refactor controller checks

--- src/main/java/org/g_node/converter/ConvController.java
+++ src/main/java/org/g_node/converter/ConvController.java
@@ -11,21 +11,19 @@
 package org.g_node.converter;
 
 import com.hp.hpl.jena.rdf.model.Model;
-import java.util.List;
 import java.util.Locale;
 import java.util.Set;
-import java.util.stream.Collectors;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.jena.riot.RiotException;
 import org.apache.log4j.Logger;
 import org.g_node.Controller;
-import org.g_node.srv.FileService;
+import org.g_node.srv.CtrlCheckService;
 import org.g_node.srv.RDFService;
 
 /**
- * Command class for the RDF to RDF converter.
+ * Controller class for the RDF to RDF converter.
  *
  * @author Michael Sonntag (sonntag@bio.lmu.de)
  */
@@ -87,23 +85,22 @@ public class ConvController implements Controller {
     }
 
     /**
-     * Method to convert an input RDF file to an RDF file of a different supported RDF format.
-     * @param cmd Commandline input provided by the user
+     * Method converting data from an input RDF file to an RDF file of a different supported RDF format.
+     * @param cmd User provided {@link CommandLine} input.
      */
     public final void run(final CommandLine cmd) {
 
-        //TODO The following partially overlaps with code in the LKTController. Check if this can be unified somehow.
         final String inputFile = cmd.getOptionValue("i");
-        if (!this.existingInputFile(inputFile)) {
+        if (!CtrlCheckService.existingInputFile(inputFile)) {
             return;
         }
 
-        if (!this.supportedFileType(inputFile)) {
+        if (!CtrlCheckService.supportedFileType(inputFile)) {
             return;
         }
 
         final String outputFormat = cmd.getOptionValue("f", "TTL").toUpperCase(Locale.ENGLISH);
-        if (!this.supportedOutputFormat(outputFormat)) {
+        if (!CtrlCheckService.supportedOutputFormat(outputFormat)) {
             return;
         }
 
@@ -129,78 +126,6 @@ public class ConvController implements Controller {
         }
 
         RDFService.writeModelToFile(outputFile, convData, outputFormat);
-    }
-
-    /**
-     * Method checks if a provided file exists, logs the findings accordingly and returns corresponding boolean value.
-     * @param inputFile Path and filename of the file that is supposed to be checked for existence.
-     * @return True in case the file exists, false in case it does not.
-     */
-    private boolean existingInputFile(final String inputFile) {
-
-        boolean properFile = true;
-
-        ConvController.LOGGER.info("Checking input file...");
-        if (!FileService.checkFile(inputFile)) {
-            ConvController.LOGGER.error(
-                    String.join("", "Input file ", inputFile, " does not exist.")
-            );
-            properFile = false;
-        }
-        return properFile;
-    }
-
-    /**
-     * Method checks if a provided file is in an RDF format that is supported by this tool. The check is done by
-     * file ending only. The file ending is mapped to entries in {@link RDFService#RDF_FORMAT_EXTENSION}.
-     * @param inputFile Path and filename of the file that is supposed to be checked for the supported file type.
-     * @return True in case the file type is supported, false in case it is not.
-     */
-    private boolean supportedFileType(final String inputFile) {
-
-        boolean supportedType = true;
-
-        ConvController.LOGGER.info("Checking input format...");
-        final List<String> checkExtension = RDFService.RDF_FORMAT_EXTENSION.values()
-                .stream()
-                .map(c->c.toUpperCase(Locale.ENGLISH))
-                .collect(Collectors.toList());
-
-        if (!FileService.checkFileType(inputFile, checkExtension)) {
-            ConvController.LOGGER.error(
-                    String.join("",
-                            "Input RDF file ", inputFile, " cannot be read.",
-                            "\n\tOnly the following file formats are supported: \n\t",
-                            RDFService.RDF_FORMAT_EXTENSION.values().toString()
-                    )
-            );
-            supportedType = false;
-        }
-        return supportedType;
-    }
-
-    /**
-     * Method checks with {@link RDFService#RDF_FORMAT_MAP} if a user provided
-     * RDF output format is supported by the current tool.
-     * @param outputFormat RDF format provided by the user.
-     * @return True if the provided format is supported by the tool, false if not.
-     */
-    private boolean supportedOutputFormat(final String outputFormat) {
-
-        boolean supportedFormat = true;
-
-        ConvController.LOGGER.info("Checking output format...");
-        if (!RDFService.RDF_FORMAT_MAP.containsKey(outputFormat)) {
-            ConvController.LOGGER.error(
-                    String.join("",
-                            "Unsupported output format: '", outputFormat, "'",
-                            "\n Please use one of the following: ",
-                            RDFService.RDF_FORMAT_MAP.keySet().toString()
-                    )
-            );
-            supportedFormat = false;
-        }
-        return supportedFormat;
     }
 
 }
---
+++ src/main/java/org/g_node/srv/CtrlCheckService.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2015, German Neuroinformatics Node (G-Node)
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of the BSD License. See
+ * LICENSE file in the root of the Project.
+ */
+
+package org.g_node.srv;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Class providing checks common to converters and crawlers implemented in this service.
+ */
+public class CtrlCheckService {
+    /**
+     * Access to the main LOGGER.
+     */
+    private static final Logger LOGGER = Logger.getLogger(CtrlCheckService.class.getName());
+    /**
+     * Method checks if a provided file exists, logs the findings accordingly and returns corresponding boolean value.
+     * @param inputFile Path and filename of the file that is supposed to be checked for existence.
+     * @return True in case the file exists, false in case it does not.
+     */
+    public static boolean existingInputFile(final String inputFile) {
+
+        boolean properFile = true;
+
+        CtrlCheckService.LOGGER.info("Checking input file...");
+        if (!FileService.checkFile(inputFile)) {
+            CtrlCheckService.LOGGER.error(
+                    String.join("", "Input file ", inputFile, " does not exist.")
+            );
+            properFile = false;
+        }
+        return properFile;
+    }
+
+    /**
+     * Method checks if a provided file is in an RDF format that is supported by this tool. The check is done by
+     * file ending only. The file ending is mapped to entries in {@link RDFService#RDF_FORMAT_EXTENSION}.
+     * @param inputFile Path and filename of the file that is supposed to be checked for the supported file type.
+     * @return True in case the file type is supported, false in case it is not.
+     */
+    public static boolean supportedFileType(final String inputFile) {
+
+        boolean supportedType = true;
+
+        CtrlCheckService.LOGGER.info("Checking input format...");
+        final List<String> checkExtension = RDFService.RDF_FORMAT_EXTENSION.values()
+                .stream()
+                .map(c->c.toUpperCase(Locale.ENGLISH))
+                .collect(Collectors.toList());
+
+        if (!FileService.checkFileType(inputFile, checkExtension)) {
+            CtrlCheckService.LOGGER.error(
+                    String.join("",
+                            "Input RDF file ", inputFile, " cannot be read.",
+                            "\n\tOnly the following file formats are supported: \n\t",
+                            RDFService.RDF_FORMAT_EXTENSION.values().toString()
+                    )
+            );
+            supportedType = false;
+        }
+        return supportedType;
+    }
+
+    /**
+     * Method checks with {@link RDFService#RDF_FORMAT_MAP} if a user provided
+     * RDF output format is supported by the current tool.
+     * @param outputFormat RDF format provided by the user.
+     * @return True if the provided format is supported by the tool, false if not.
+     */
+    public static boolean supportedOutputFormat(final String outputFormat) {
+
+        boolean supportedFormat = true;
+
+        CtrlCheckService.LOGGER.info("Checking output format...");
+        if (!RDFService.RDF_FORMAT_MAP.containsKey(outputFormat)) {
+            CtrlCheckService.LOGGER.error(
+                    String.join("",
+                            "Unsupported output format: '", outputFormat, "'",
+                            "\n Please use one of the following: ",
+                            RDFService.RDF_FORMAT_MAP.keySet().toString()
+                    )
+            );
+            supportedFormat = false;
+        }
+        return supportedFormat;
+    }
+
+}
