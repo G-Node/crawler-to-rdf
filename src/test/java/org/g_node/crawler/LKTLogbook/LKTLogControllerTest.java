2392d134c333cb5cfee16baa9aedd5f76999afa4
M. Sonntag <michael.p.sonntag@gmail.com>
2015-11-13 14:47:11 +0100

    incrTestCov: add LKT tool cli options test

---
+++ src/test/java/org/g_node/crawler/LKTLogbook/LKTLogControllerTest.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2015, German Neuroinformatics Node (G-Node)
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of the BSD License. See
+ * LICENSE file in the root of the Project.
+ */
+
+package org.g_node.crawler.LKTLogbook;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit tests for the {@link LKTLogController} class.
+ *
+ * @author Michael Sonntag (sonntag@bio.lmu.de)
+ */
+public class LKTLogControllerTest {
+
+    private LKTLogController logCtrl = new LKTLogController(new LKTLogParser());
+
+    /**
+     * Tests for the {@link CommandLine} {@link Options} of the RDF to RDF converter tool.
+     * @throws Exception
+     */
+    @Test
+    public void testOptions() throws Exception {
+        Set<String> testRegToolsSet = new HashSet<>(Arrays.asList("one", "two"));
+        Options checkOpt = this.logCtrl.options(testRegToolsSet);
+
+        assertThat(checkOpt.getOptions().size()).isEqualTo(4);
+
+        assertThat(checkOpt.hasOption("-i")).isTrue();
+        assertThat(checkOpt.hasLongOption("in-file")).isTrue();
+        assertThat(checkOpt.getOption("-i").isRequired()).isTrue();
+
+        assertThat(checkOpt.hasOption("-o")).isTrue();
+        assertThat(checkOpt.hasLongOption("out-file")).isTrue();
+        assertThat(checkOpt.getOption("-o").isRequired()).isFalse();
+
+        assertThat(checkOpt.hasOption("-f")).isTrue();
+        assertThat(checkOpt.hasLongOption("out-format")).isTrue();
+        assertThat(checkOpt.getOption("-f").isRequired()).isFalse();
+
+        assertThat(checkOpt.hasOption("-h")).isTrue();
+    }
+
+}
