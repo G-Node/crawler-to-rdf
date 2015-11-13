3f199545215cd84f9f9445257fb0d604493e3b62
M. Sonntag <michael.p.sonntag@gmail.com>
2015-11-13 22:11:29 +0100

    incrTestCov: add LKT parser tests

--- src/test/java/org/g_node/crawler/LKTLogbook/LKTLogControllerTest.java
+++ src/test/java/org/g_node/crawler/LKTLogbook/LKTLogControllerTest.java
@@ -20,7 +20,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import javax.swing.table.DefaultTableModel;
-
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -31,8 +30,8 @@ import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
-import org.g_node.converter.ConvController;
 import org.jopendocument.dom.spreadsheet.SpreadSheet;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -44,15 +43,55 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class LKTLogControllerTest {
 
-    private LKTLogController logCtrl = new LKTLogController(new LKTLogParser());
+    private LKTLogController logCtrl;
     private Set<String> logSet = new HashSet<>(Arrays.asList("one", "two"));
+    private PrintStream stdout;
+    private ByteArrayOutputStream outStream;
+    private Logger rootLogger;
+
+    private Path testFileFolder;
+    private File currTestFile;
 
     /**
      * Ensure a fresh {@link LKTLogController} at the beginning of each test.
+     * Set up Logger and tmp folder.
+     * @throws Exception
      */
     @Before
-    public void provideStuff() {
+    public void setUp() throws Exception {
         this.logCtrl = new LKTLogController(new LKTLogParser());
+
+        this.stdout = System.out;
+        this.outStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(this.outStream));
+
+        this.rootLogger = Logger.getRootLogger();
+        this.rootLogger.setLevel(Level.INFO);
+        this.rootLogger.addAppender(
+                new ConsoleAppender(
+                        new PatternLayout("[%-5p] %m%n")
+                )
+        );
+
+        final String tmpRoot = System.getProperty("java.io.tmpdir");
+        final String testFolderName = "fileservicetest";
+        final String testFileName = "test.txt";
+        this.testFileFolder = Paths.get(tmpRoot, testFolderName);
+        this.currTestFile = this.testFileFolder.resolve(testFileName).toFile();
+        FileUtils.write(this.currTestFile, "This is a normal test file");
+    }
+
+    /**
+     * Delete temporary test files and folders, close Logger and reset std out.
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        if (Files.exists(this.testFileFolder)) {
+            FileUtils.deleteDirectory(this.testFileFolder.toFile());
+        }
+        this.rootLogger.removeAllAppenders();
+        System.setOut(this.stdout);
     }
 
     /**
@@ -81,42 +120,16 @@ public class LKTLogControllerTest {
     }
 
     /**
-     * Tests the run method of the {@link ConvController}.
+     * Tests the run method of the {@link LKTLogController}.
+     * Will test LKTLogParser, LKTLogParserSheet and LKTLogParserEntry as well with an invalid ods sheet.
      * @throws Exception
      */
     @Test
     public void runTest() throws Exception {
-        // TODO check if this should be split up into different tests.
-
-        // Set up environment
-        final PrintStream stdout = System.out;
-        final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outStream));
-        final PrintStream errout = System.err;
-        final ByteArrayOutputStream errStream = new ByteArrayOutputStream();
-        System.setErr(new PrintStream(errStream));
-
-        final Logger rootLogger = Logger.getRootLogger();
-        rootLogger.setLevel(Level.INFO);
-        rootLogger.addAppender(
-                new ConsoleAppender(
-                        new PatternLayout("[%-5p] %m%n")
-                )
-        );
-
-        // Create test files
-        final String tmpRoot = System.getProperty("java.io.tmpdir");
-        final String testFolderName = "fileservicetest";
-        final String testFileName = "test.txt";
-        final Path testFileFolder = Paths.get(tmpRoot, testFolderName);
-        final File currTestFile = testFileFolder.resolve(testFileName).toFile();
-        FileUtils.write(currTestFile, "This is a normal test file");
-
         final String testODSFileName = "test.ods";
-        final File currODSTestFile = testFileFolder.resolve(testODSFileName).toFile();
+        final File currODSTestFile = this.testFileFolder.resolve(testODSFileName).toFile();
         final File ods = new File(currODSTestFile.toString());
         SpreadSheet.createEmpty(new DefaultTableModel()).saveAs(ods);
-
 
         final CommandLineParser parser = new DefaultParser();
         final Options useOptions = this.logCtrl.options(this.logSet);
@@ -139,20 +152,20 @@ public class LKTLogControllerTest {
         cmd = parser.parse(useOptions, args, false);
         this.logCtrl.run(cmd);
 
-        assertThat(outStream.toString()).contains("Input file iDoNotExist does not exist");
-        outStream.reset();
+        assertThat(this.outStream.toString()).contains("Input file iDoNotExist does not exist");
+        this.outStream.reset();
 
         // Test existing input file, unsupported file type
         args = new String[2];
         args[0] = "-i";
-        args[1] = currTestFile.toString();
+        args[1] = this.currTestFile.toString();
         cmd = parser.parse(useOptions, args, false);
         this.logCtrl.run(cmd);
 
-        assertThat(outStream.toString()).contains(
-                String.join("", "[ERROR] Input RDF file ", currTestFile.toString(), " cannot be read.")
+        assertThat(this.outStream.toString()).contains(
+                String.join("", "[ERROR] Input RDF file ", this.currTestFile.toString(), " cannot be read.")
         );
-        outStream.reset();
+        this.outStream.reset();
 
         // Test provide supported input file type, unsupported output RDF format
         args = new String[4];
@@ -163,8 +176,8 @@ public class LKTLogControllerTest {
         cmd = parser.parse(useOptions, args, false);
         this.logCtrl.run(cmd);
 
-        assertThat(outStream.toString()).contains("[ERROR] Unsupported output format: 'IDONOTEXIST'");
-        outStream.reset();
+        assertThat(this.outStream.toString()).contains("[ERROR] Unsupported output format: 'IDONOTEXIST'");
+        this.outStream.reset();
 
         // Test provide supported input file type, w/o proper entries.
         args = new String[2];
@@ -173,19 +186,58 @@ public class LKTLogControllerTest {
         cmd = parser.parse(useOptions, args, false);
         this.logCtrl.run(cmd);
 
-        assertThat(outStream.toString()).contains(
+        assertThat(this.outStream.toString()).contains(
                 String.join("",
                         "[Parser] sheet null does not contain valid data.")
         );
-        outStream.reset();
+        this.outStream.reset();
 
-        // Clean up
-        if (Files.exists(testFileFolder)) {
-            FileUtils.deleteDirectory(testFileFolder.toFile());
-        }
-        rootLogger.removeAllAppenders();
-        System.setOut(stdout);
-        System.setErr(errout);
+        // Test use custom invalid test file.
+        final String invODSTestFile = this.getClass().getResource("/lkt_test_invalid.ods").getPath();
+
+        args = new String[2];
+        args[0] = "-i";
+        args[1] = invODSTestFile;
+
+        cmd = parser.parse(useOptions, args, false);
+        this.logCtrl.run(cmd);
+
+        assertThat(this.outStream.toString())
+                .contains("There are parser errors present. Please resolve them and run the program again.");
+        this.outStream.reset();
     }
 
+    /**
+     * Tests the run method of the {@link LKTLogController} with a valid ods file.
+     * Will test LKTLogParser, LKTLogParserSheet, LKTLogParserEntry and LKTLogToRDF as well.
+     * @throws Exception
+     */
+    @Test
+    public void runTestValidFile() throws Exception {
+        final File currOutFile = this.testFileFolder.resolve("out.ttl").toFile();
+        FileUtils.write(currOutFile, "");
+
+        final CommandLineParser parser = new DefaultParser();
+        final Options useOptions = this.logCtrl.options(this.logSet);
+        String[] args;
+        CommandLine cmd;
+
+        final String validODSTestFile = this.getClass().getResource("/lkt_test.ods").getPath();
+        args = new String[4];
+        args[0] = "-i";
+        args[1] = validODSTestFile;
+        args[2] = "-o";
+        args[3] = this.testFileFolder.resolve("out.ttl").toString();
+
+        cmd = parser.parse(useOptions, args, false);
+        this.logCtrl.run(cmd);
+
+        assertThat(this.outStream.toString())
+                .contains(
+                        String.join("", "Writing data to RDF file, ",
+                                this.testFileFolder.resolve("out.ttl").toString(),
+                                " using format 'TTL'")
+                );
+        this.outStream.reset();
+    }
 }
